4310a9dc2d82be6dae488a521d77aa047b4c3972
MokrySzczupak <norbi2790@gmail.com>
2021-05-03 11:51:39 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "f7bfa6ba-6bd5-498c-ae11-2499181e497e";
+export const MODAL_CLOSE_SEC = 2.5;
